715a24ca2edff4b7ecac137c4a22bdf149396935
Jason Chen CJ <jason.cj.chen@intel.com>
2022-12-08 02:26:47 +0800

    KVM: VMX: Add initialization/teardown for shadow vm/vcpu
    Add initialization/teardown for shadow vm & shadow vcpu from corresponding
    kvm_x86_ops.
    
    The initialization allocates shadow vm or shadow vcpu data structure
    according to the size exposed from pkvm_constants, then hypercall to
    send such data structure's address to pKVM.
    
    The teardown hypercall to pKVM asking for the teardown of corresponding
    shadow vm or shadow vcpu data structure in the hypervisor, then do finally
    free of related memory.
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    (cherry picked from commit 37f89c71e85846334a270a6b47452f428d27bd0a)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -49,6 +49,7 @@
 #include <asm/mwait.h>
 #include <asm/spec-ctrl.h>
 #include <asm/vmx.h>
+#include <asm/kvm_pkvm.h>
 
 #include <trace/events/ipi.h>
 
@@ -7528,6 +7529,8 @@ void vmx_vcpu_free(struct kvm_vcpu *vcpu)
 	nested_vmx_free_vcpu(vcpu);
 	free_loaded_vmcs(vmx->loaded_vmcs);
 	free_page((unsigned long)vmx->ve_info);
+
+	pkvm_teardown_shadow_vcpu(vcpu);
 }
 
 int vmx_vcpu_create(struct kvm_vcpu *vcpu)
@@ -7639,7 +7642,7 @@ int vmx_vcpu_create(struct kvm_vcpu *vcpu)
 		WRITE_ONCE(to_kvm_vmx(vcpu->kvm)->pid_table[vcpu->vcpu_id],
 			   __pa(&vmx->pi_desc) | PID_TABLE_ENTRY_VALID);
 
-	return 0;
+	return pkvm_init_shadow_vcpu(vcpu);
 
 free_vmcs:
 	free_loaded_vmcs(vmx->loaded_vmcs);
@@ -7681,7 +7684,13 @@ int vmx_vm_init(struct kvm *kvm)
 			break;
 		}
 	}
-	return 0;
+
+	return pkvm_init_shadow_vm(kvm);
+}
+
+void vmx_vm_free(struct kvm *kvm)
+{
+	pkvm_teardown_shadow_vm(kvm);
 }
 
 static inline bool vmx_ignore_guest_pat(struct kvm *kvm)
