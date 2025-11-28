887ed44a213e167ee40a1c926f71fc90576c03ab
Markku Ahvenjärvi <mankku@gmail.com>
2025-11-28 10:25:58 +0200

    pkvm: x86: fix vmx debug build errors
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- arch/x86/kvm/vmx/vmx_debug.h
+++ arch/x86/kvm/vmx/vmx_debug.h
@@ -6,12 +6,19 @@
 char *debug_dump_vmcs(void);
 char *debug_dump_vmx_msr_state(void);
 void debug_validate_vmcs_registers(void);
-#define debug_validate_vmcs debug_validate_vmcs_registers
+
+#if !defined(CONFIG_PKVM_INTEL_DEBUG) && defined(__PKVM_HYP__)
+#define debug_validate_vmcs(...) do {} while (0)
 #else
-#define debug_validate_vmcs
-static inline void char *debug_dump_vmcs(void) { return NULL; }
-static inline void char *debug_dump_vmx_msr_state(void) { return NULL; }
-static inline void debug_validate_vmcs_registers(void) { return NULL; }
+#define debug_validate_vmcs debug_validate_vmcs_registers
+#endif
+
+#else
+static inline char *debug_dump_vmcs(void) { return NULL; }
+static inline char *debug_dump_vmx_msr_state(void) { return NULL; }
+static inline debug_validate_vmcs_registers(void) { }
+
+#define debug_validate_vmcs(...) do {} while (0)
 #endif
 
 #endif /* __KVM_X86_VMX_DEBUG_H */
