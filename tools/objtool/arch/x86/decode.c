6a74748f5fe264057eaa9639bdbfacefc1a2e026
Markku Ahvenjärvi <mankku@gmail.com>
2025-11-28 10:33:36 +0200

    pkvm: x86: fix objtool retpoline checks
    Fixes following build error when CONFIG_PKVM_INTEL_DEBUG is not set:
    
    incomplete ORC unwind tables in file: vmlinux
    
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- tools/objtool/arch/x86/decode.c
+++ tools/objtool/arch/x86/decode.c
@@ -845,5 +845,7 @@ bool arch_is_rethunk(struct symbol *sym)
 bool arch_is_embedded_insn(struct symbol *sym)
 {
 	return !strcmp(sym->name, "retbleed_return_thunk") ||
-	       !strcmp(sym->name, "srso_safe_ret");
+	       !strcmp(sym->name, "srso_safe_ret") ||
+	       !strcmp(sym->name, "__pkvm_retbleed_return_thunk") ||
+	       !strcmp(sym->name, "__pkvm_srso_safe_ret");
 }
