YadongQi/linux-intel-lts2021-chromium

pkvm: x86: init: isolate IOMMU from host VM
As IOMMU is maintained by pkvm, the IOMMU MMIO mapping in EPT needs
to be removed to isolate the IOMMU from host VM, so that the IOMMU MMIO
accessing from the host IOMMU driver can be captured by pkvm and
emulate. The isolation is done in the finalise hypercall.

Normally, after remove the MMIO accessing from EPT, such access will
trigger EPT violation vmexit then doing instruction & IO emulation in
the hypervisor. While we don't expect to do instruction emulation in
pkvm hypervisor, so to simplify the IOMMU MMIO vmexit, introduced the
pkvm_read/write helpper functions, which relies on pkvm_enabled flag to
determine the way of accessing the MMIO. If pkvm_enabled is true,
uses the hypercall to access MMIO. And if pkvm_enabled is false, fallback
to the traditional way to do the read and write.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm/hyp/iommu.c
@@ -1,13 +1,24 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/intel-iommu.h>
+#include <pkvm_spinlock.h>
 #include <pkvm.h>
 #include "pkvm_hyp.h"
 #include "memory.h"
 #include "mmu.h"
+#include "ept.h"
+#include "debug.h"
 
 struct pkvm_iommu {
 	struct intel_iommu iommu;
+	pkvm_spinlock_t lock;
+	bool activated;
 };
+
+#define for_each_valid_iommu(p)					\
+	for (p = iommus; p < iommus + PKVM_MAX_IOMMU_NUM; p++)	\
+		if (!p || !p->iommu.reg_phys) {			\
+			continue;				\
+		} else
 
 static struct pkvm_iommu iommus[PKVM_MAX_IOMMU_NUM];
 
@@ -21,6 +32,7 @@ int pkvm_init_iommu(void)
 		if (!info->reg_phys)
 			break;
 
+		pkvm_spinlock_init(&piommu->lock);
 		piommu->iommu.reg_phys = info->reg_phys;
 		piommu->iommu.reg_size = info->reg_size;
 		piommu->iommu.reg = pkvm_iophys_to_virt(info->reg_phys);
@@ -37,7 +49,97 @@ int pkvm_init_iommu(void)
 
 		piommu->iommu.cap = readq(piommu->iommu.reg + DMAR_CAP_REG);
 		piommu->iommu.ecap = readq(piommu->iommu.reg + DMAR_ECAP_REG);
+
+		ret = pkvm_host_ept_unmap((unsigned long)info->reg_phys,
+				     (unsigned long)info->reg_phys,
+				     info->reg_size);
+		if (ret)
+			return ret;
 	}
 
 	return 0;
+}
+
+static struct pkvm_iommu *find_iommu_by_reg_phys(unsigned long phys)
+{
+	struct pkvm_iommu *iommu;
+
+	for_each_valid_iommu(iommu) {
+		if ((phys >= iommu->iommu.reg_phys) &&
+			(phys < (iommu->iommu.reg_phys + iommu->iommu.reg_size)))
+			return iommu;
+	}
+
+	return NULL;
+}
+
+static unsigned long direct_access_iommu_mmio(struct pkvm_iommu *iommu,
+					      bool is_read, int len,
+					      unsigned long phys,
+					      unsigned long val)
+{
+	unsigned long offset = phys - iommu->iommu.reg_phys;
+	void *reg = iommu->iommu.reg + offset;
+	unsigned long ret = 0;
+
+	switch (len) {
+	case 4:
+		if (is_read)
+			ret = (unsigned long)readl(reg);
+		else
+			writel((u32)val, reg);
+		break;
+	case 8:
+		if (is_read)
+			ret = (unsigned long)readq(reg);
+		else
+			writeq((u64)val, reg);
+		break;
+	default:
+		pkvm_err("%s: %s: unsupported len %d\n", __func__,
+			 is_read ? "read" : "write", len);
+		break;
+	}
+
+	return ret;
+}
+
+static unsigned long access_iommu_mmio(struct pkvm_iommu *iommu, bool is_read,
+				       int len, unsigned long phys,
+				       unsigned long val)
+{
+	unsigned long offset = phys - iommu->iommu.reg_phys;
+	unsigned long ret = 0;
+
+	/* pkvm IOMMU driver is not activated yet, so directly access MMIO */
+	if (unlikely(!iommu->activated))
+		return direct_access_iommu_mmio(iommu, is_read, len, phys, val);
+
+	/* Only need to emulate part of the MMIO */
+	switch (offset) {
+	default:
+		/* Not emulated MMIO can directly goes to hardware */
+		ret = direct_access_iommu_mmio(iommu, is_read, len, phys, val);
+		break;
+	}
+
+	return ret;
+}
+
+unsigned long pkvm_access_iommu(bool is_read, int len, unsigned long phys, unsigned long val)
+{
+	struct pkvm_iommu *pkvm_iommu = find_iommu_by_reg_phys(phys);
+	unsigned long ret;
+
+	if (!pkvm_iommu) {
+		pkvm_err("%s: cannot find pkvm iommu for reg 0x%lx\n",
+			__func__, phys);
+		return 0;
+	}
+
+	pkvm_spin_lock(&pkvm_iommu->lock);
+	ret = access_iommu_mmio(pkvm_iommu, is_read, len, phys, val);
+	pkvm_spin_unlock(&pkvm_iommu->lock);
+
+	return ret;
 }

elmankku/pKVM-x86-IA

(cherry picked from commit 00e20b10eef92d9de57709566d085f320bb2c0a6)
Signed-off-by: Vineeth Pillai <vineethrp@google.com>

arch/x86/kvm/vmx/pkvm/hyp/iommu.c
@@ -3,14 +3,25 @@
  * Copyright (C) 2022 Intel Corporation
  */
 #include <../drivers/iommu/intel/iommu.h>
+#include <asm/pkvm_spinlock.h>
 #include <pkvm.h>
 #include "pkvm_hyp.h"
 #include "memory.h"
 #include "mmu.h"
+#include "ept.h"
+#include "debug.h"
 
 struct pkvm_iommu {
 	struct intel_iommu iommu;
+	pkvm_spinlock_t lock;
+	bool activated;
 };
+
+#define for_each_valid_iommu(p)					\
+	for (p = iommus; p < iommus + PKVM_MAX_IOMMU_NUM; p++)	\
+		if (!p || !p->iommu.reg_phys) {			\
+			continue;				\
+		} else
 
 static struct pkvm_iommu iommus[PKVM_MAX_IOMMU_NUM];
 
@@ -24,6 +35,7 @@ int pkvm_init_iommu(void)
 		if (!info->reg_phys)
 			break;
 
+		pkvm_spin_lock_init(&piommu->lock);
 		piommu->iommu.reg_phys = info->reg_phys;
 		piommu->iommu.reg_size = info->reg_size;
 		piommu->iommu.reg = pkvm_iophys_to_virt(info->reg_phys);
@@ -40,7 +52,97 @@ int pkvm_init_iommu(void)
 
 		piommu->iommu.cap = readq(piommu->iommu.reg + DMAR_CAP_REG);
 		piommu->iommu.ecap = readq(piommu->iommu.reg + DMAR_ECAP_REG);
+
+		ret = pkvm_host_ept_unmap((unsigned long)info->reg_phys,
+				     (unsigned long)info->reg_phys,
+				     info->reg_size);
+		if (ret)
+			return ret;
 	}
 
 	return 0;
+}
+
+static struct pkvm_iommu *find_iommu_by_reg_phys(unsigned long phys)
+{
+	struct pkvm_iommu *iommu;
+
+	for_each_valid_iommu(iommu) {
+		if ((phys >= iommu->iommu.reg_phys) &&
+			(phys < (iommu->iommu.reg_phys + iommu->iommu.reg_size)))
+			return iommu;
+	}
+
+	return NULL;
+}
+
+static unsigned long direct_access_iommu_mmio(struct pkvm_iommu *iommu,
+					      bool is_read, int len,
+					      unsigned long phys,
+					      unsigned long val)
+{
+	unsigned long offset = phys - iommu->iommu.reg_phys;
+	void *reg = iommu->iommu.reg + offset;
+	unsigned long ret = 0;
+
+	switch (len) {
+	case 4:
+		if (is_read)
+			ret = (unsigned long)readl(reg);
+		else
+			writel((u32)val, reg);
+		break;
+	case 8:
+		if (is_read)
+			ret = (unsigned long)readq(reg);
+		else
+			writeq((u64)val, reg);
+		break;
+	default:
+		pkvm_err("%s: %s: unsupported len %d\n", __func__,
+			 is_read ? "read" : "write", len);
+		break;
+	}
+
+	return ret;
+}
+
+static unsigned long access_iommu_mmio(struct pkvm_iommu *iommu, bool is_read,
+				       int len, unsigned long phys,
+				       unsigned long val)
+{
+	unsigned long offset = phys - iommu->iommu.reg_phys;
+	unsigned long ret = 0;
+
+	/* pkvm IOMMU driver is not activated yet, so directly access MMIO */
+	if (unlikely(!iommu->activated))
+		return direct_access_iommu_mmio(iommu, is_read, len, phys, val);
+
+	/* Only need to emulate part of the MMIO */
+	switch (offset) {
+	default:
+		/* Not emulated MMIO can directly goes to hardware */
+		ret = direct_access_iommu_mmio(iommu, is_read, len, phys, val);
+		break;
+	}
+
+	return ret;
+}
+
+unsigned long pkvm_access_iommu(bool is_read, int len, unsigned long phys, unsigned long val)
+{
+	struct pkvm_iommu *pkvm_iommu = find_iommu_by_reg_phys(phys);
+	unsigned long ret;
+
+	if (!pkvm_iommu) {
+		pkvm_err("%s: cannot find pkvm iommu for reg 0x%lx\n",
+			__func__, phys);
+		return 0;
+	}
+
+	pkvm_spin_lock(&pkvm_iommu->lock);
+	ret = access_iommu_mmio(pkvm_iommu, is_read, len, phys, val);
+	pkvm_spin_unlock(&pkvm_iommu->lock);
+
+	return ret;
 }