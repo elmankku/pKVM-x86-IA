intel-staging/pKVM-IA

pkvm: x86: Add pkvm guest virtualization exception (#VE)
This patch includes the guest support for the virtualization exception.
When guest do the MMIO operation, guest will receive #VE and jump to the
exception handler.

In the #VE handler, guest will first use hypercall to get #VE
information, and then it will do instruction decode by itself. And using
hypercall to pass the mmio information to KVM to emulate it.

After KVM emulate this MMIO, guest will return from the #VE handler.
After it return, guest execute the MMIO operation success, and continue
to run.

Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>

arch/x86/coco/pkvm/pkvm.c
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: GPL-2.0
-#include <asm/pkvm.h>
+
+#undef pr_fmt
+#define pr_fmt(fmt)     "pkvm: " fmt
+
+#include <linux/cpufeature.h>
 #include <linux/kvm_para.h>
+#include <asm/coco.h>
+#include <asm/vmx.h>
+#include <asm/pkvm.h>
+#include <asm/insn.h>
+#include <asm/insn-eval.h>
+#include <asm/pgtable.h>
+#include <asm/virt_exception.h>
 
 static bool pkvm_guest_detected;
 
@@ -36,6 +47,59 @@ int pkvm_set_mem_host_visibility(unsigned long addr, int numpages, bool enc)
 	return 0;
 }
 
+void pkvm_get_ve_info(struct ve_info *ve)
+{
+	/* Reuse the tdx output for pkvm. */
+	struct tdx_module_output out;
+
+	__pkvm_module_call(PKVM_GHC_GET_VE_INFO, &out);
+
+	/* Transfer the output parameters */
+	ve->exit_reason = out.rcx;
+	ve->exit_qual   = out.rdx;
+	ve->gla         = out.r8;
+	ve->gpa         = out.r9;
+}
+
+static bool mmio_write(int size, unsigned long addr, unsigned long val)
+{
+	kvm_hypercall3(PKVM_GHC_IOWRITE, addr, size, val);
+
+	return true;
+}
+
+static bool mmio_read(int size, unsigned long addr, unsigned long *val)
+{
+	*val = kvm_hypercall2(PKVM_GHC_IOREAD, addr, size);
+
+	return true;
+}
+
+static int virt_exception_kernel(struct pt_regs *regs, struct ve_info *ve)
+{
+	switch (ve->exit_reason) {
+	case EXIT_REASON_EPT_VIOLATION:
+		return ve_handle_mmio(regs, ve);
+	default:
+		pr_warn("Unexpected #VE: %lld\n", ve->exit_reason);
+		return -EIO;
+	}
+}
+
+static bool pkvm_handle_virt_exception(struct pt_regs *regs, struct ve_info *ve)
+{
+	int insn_len;
+
+	insn_len = virt_exception_kernel(regs, ve);
+	if (insn_len < 0)
+		return false;
+
+	/* After successful #VE handling, move the IP */
+	regs->ip += insn_len;
+
+	return true;
+}
+
 __init void pkvm_guest_early_init(void)
 {
 	u32 sig;
@@ -56,5 +120,10 @@ __init void pkvm_guest_early_init(void)
 
 	pkvm_guest_detected = true;
 
-	pr_info("pkvm guest detected\n");
+	ve_x86_ops.mmio_read = mmio_read;
+	ve_x86_ops.mmio_write = mmio_write;
+	ve_x86_ops.handle_virt_exception = pkvm_handle_virt_exception;
+	ve_x86_ops.get_ve_info = pkvm_get_ve_info;
+
+	pr_info("Guest detected\n");
 }

arch/x86/include/asm/pkvm.h
@@ -8,6 +8,7 @@
 #include <asm/kvm_para.h>
 #include <asm/io.h>
 #include <asm/coco.h>
+#include <asm/virt_exception.h>
 
 /* PKVM Hypercalls */
 #define PKVM_HC_INIT_FINALISE		1
@@ -135,6 +136,8 @@ static inline void pkvm_update_iommu_virtual_caps(u64 *cap, u64 *ecap)
 void pkvm_guest_early_init(void);
 bool pkvm_is_protected_guest(void);
 int pkvm_set_mem_host_visibility(unsigned long addr, int numpages, bool enc);
+
+u64 __pkvm_module_call(u64 fn, struct tdx_module_output *out);
 
 #else
 

elmankku/pKVM-x86-IA

(cherry picked from commit 924a26dfeeecef477960fc2db3bf0cb229d9b20a)
Signed-off-by: Vineeth Pillai <vineethrp@google.com>

arch/x86/coco/pkvm/pkvm.c
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: GPL-2.0
-#include <asm/pkvm.h>
+
+#undef pr_fmt
+#define pr_fmt(fmt)     "pkvm: " fmt
+
+#include <linux/cpufeature.h>
 #include <linux/kvm_para.h>
+#include <asm/coco.h>
+#include <asm/vmx.h>
+#include <asm/pkvm.h>
+#include <asm/insn.h>
+#include <asm/insn-eval.h>
+#include <asm/pgtable.h>
+#include <asm/virt_exception.h>
 
 static bool pkvm_guest_detected;
 
@@ -36,9 +47,67 @@ int pkvm_set_mem_host_visibility(unsigned long addr, int numpages, bool enc)
 	return 0;
 }
 
+void pkvm_get_ve_info(struct ve_info *ve)
+{
+	/* Reuse the tdx output for pkvm. */
+	struct tdx_module_args out;
+
+	__pkvm_module_call(PKVM_GHC_GET_VE_INFO, &out);
+
+	/* Transfer the output parameters */
+	ve->exit_reason = out.rcx;
+	ve->exit_qual   = out.rdx;
+	ve->gla         = out.r8;
+	ve->gpa         = out.r9;
+}
+
+static bool mmio_write(int size, unsigned long addr, unsigned long val)
+{
+	kvm_hypercall3(PKVM_GHC_IOWRITE, addr, size, val);
+
+	return true;
+}
+
+static bool mmio_read(int size, unsigned long addr, unsigned long *val)
+{
+	*val = kvm_hypercall2(PKVM_GHC_IOREAD, addr, size);
+
+	return true;
+}
+
+static int virt_exception_kernel(struct pt_regs *regs, struct ve_info *ve)
+{
+	switch (ve->exit_reason) {
+	case EXIT_REASON_EPT_VIOLATION:
+		return ve_handle_mmio(regs, ve);
+	default:
+		pr_warn("Unexpected #VE: %lld\n", ve->exit_reason);
+		return -EIO;
+	}
+}
+
+static bool pkvm_handle_virt_exception(struct pt_regs *regs, struct ve_info *ve)
+{
+	int insn_len;
+
+	insn_len = virt_exception_kernel(regs, ve);
+	if (insn_len < 0)
+		return false;
+
+	/* After successful #VE handling, move the IP */
+	regs->ip += insn_len;
+
+	return true;
+}
+
 __init void pkvm_guest_init_coco(void)
 {
 	cc_set_vendor(CC_VENDOR_PKVM);
 
 	pkvm_guest_detected = true;
+
+	ve_x86_ops.mmio_read = mmio_read;
+	ve_x86_ops.mmio_write = mmio_write;
+	ve_x86_ops.handle_virt_exception = pkvm_handle_virt_exception;
+	ve_x86_ops.get_ve_info = pkvm_get_ve_info;
 }

arch/x86/include/asm/pkvm.h
@@ -8,6 +8,7 @@
 #include <asm/kvm_para.h>
 #include <asm/io.h>
 #include <asm/coco.h>
+#include <asm/virt_exception.h>
 
 /* PKVM Hypercalls */
 #define PKVM_HC_INIT_FINALISE		1
@@ -135,6 +136,8 @@ static inline void pkvm_update_iommu_virtual_caps(u64 *cap, u64 *ecap)
 void pkvm_guest_init_coco(void);
 bool pkvm_is_protected_guest(void);
 int pkvm_set_mem_host_visibility(unsigned long addr, int numpages, bool enc);
+
+u64 __pkvm_module_call(u64 fn, struct tdx_module_args *out);
 
 #else
 